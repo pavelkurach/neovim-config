5a783409e05bc549db5d3692c8eb171152ae0576
Pavel Kurach <pavel.kurach1@gmail.com>
2024-11-17 15:19:35 +0100

    wip

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -208,12 +208,8 @@ return {
     "hedyhli/outline.nvim",
     lazy = false,
     config = function()
-      -- Example mapping to toggle outline
       vim.keymap.set("n", "<leader>o", "<cmd>Outline<CR>", { desc = "Toggle Outline" })
-
-      require("outline").setup {
-        -- Your setup opts here (leave empty to use defaults)
-      }
+      require("outline").setup {}
     end,
   },
 
@@ -232,46 +228,10 @@ return {
     event = "VeryLazy",
     lazy = false,
     version = false, -- set this if you want to always pull the latest change
-    opts = {
-      provider = "copilot", -- Recommend using Claude
-      auto_suggestions_provider = "copilot",
-    },
-    -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+    opts = require("configs.avante").opts,
     build = "make",
-    -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-      "stevearc/dressing.nvim",
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      --- The below dependencies are optional,
-      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
-      "zbirenbaum/copilot.lua", -- for providers='copilot'
-      {
-        -- support for image pasting
-        "HakonHarnes/img-clip.nvim",
-        event = "VeryLazy",
-        opts = {
-          -- recommended settings
-          default = {
-            embed_image_as_base64 = false,
-            prompt_for_file_name = false,
-            drag_and_drop = {
-              insert_mode = true,
-            },
-            -- required for Windows users
-            use_absolute_path = true,
-          },
-        },
-      },
-      {
-        -- Make sure to set this up properly if you have lazy=true
-        "MeanderingProgrammer/render-markdown.nvim",
-        opts = {
-          file_types = { "markdown", "Avante" },
-        },
-        ft = { "markdown", "Avante" },
-      },
-    },
+    dependencies = require("configs.avante").dependencies,
   },
+
+  { "akinsho/git-conflict.nvim", version = "*", config = true, lazy = false },
 }
