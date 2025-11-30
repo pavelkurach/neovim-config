d1ead09b4c70e1841d0703b15396a06bdbf02e42
Pavel Kurach <pavel.kurach1@gmail.com>
2025-12-01 00:23:44 +0100

    feat: transparent inlay hints

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -1,1 +1,14 @@
 require "nvchad.autocmds"
+
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local function opts(desc)
+      return { buffer = args.buf, desc = "LSP " .. desc }
+    end
+
+    vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts "Go to type definition")
+    vim.keymap.set("n", "gh", vim.lsp.buf.hover, opts "Hover information")
+
+    vim.api.nvim_set_hl(0, "LspInlayHint", { bg = "NONE", fg = "#666666" })
+  end,
+})
--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,15 +1,4 @@
 require("nvchad.configs.lspconfig").defaults()
 
-vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(args)
-    local function opts(desc)
-      return { buffer = args.buf, desc = "LSP " .. desc }
-    end
-
-    vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts "Go to type definition")
-    vim.keymap.set("n", "gh", vim.lsp.buf.hover, opts "Hover information")
-  end,
-})
-
 local servers = { "html", "cssls", "basedpyright", "clangd" }
 vim.lsp.enable(servers)
